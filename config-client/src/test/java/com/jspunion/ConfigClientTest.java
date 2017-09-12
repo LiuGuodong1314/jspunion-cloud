bc95a2989cbcafb12d60e184afd59faee06f8bee
Liu Guodong <liuguodong1413@sina.com>
2017-09-12 13:50:22 +0800

    ok

--- config-client/src/test/java/com/jspunion/ConfigClientTest.java
+++ config-client/src/test/java/com/jspunion/ConfigClientTest.java
@@ -3,16 +3,13 @@ package com.jspunion;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.HttpClientUtils;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
 public class ConfigClientTest {
 
     @Test
-    public void testPost() throws Exception{
+    public void testPost() throws Exception {
 
         HttpPost post = new HttpPost("http://localhost:8080/refresh");
         HttpClient client = HttpClients.createDefault();
@@ -20,7 +17,6 @@ public class ConfigClientTest {
 
         HttpResponse resp = client.execute(post);
         System.out.println(resp.getStatusLine().getStatusCode());
-
 
 
     }
